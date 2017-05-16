a61589591f9ea3a78ed09c591d627eb89ced9574
Mukasa Peter <mkspeter3@gmail.com>
2017-05-16 06:08:44 +0500

    Assignment 1
    Displaying the day the user was born

--- Display Day born.py
+++ Display Day born.py
@@ -5,7 +5,7 @@
 
 import datetime,calendar #1 importing the modules am going to use in the program ie calendar module and datetime module
 
-cur_year = int(datetime.date.today().strftime('%Y')) #2 checking the system year which is now 2017 and saved in the variable as an integer
+cur_year = int(datetime.date.today().strftime('%Y')) #2 checking and getting the system year which is now 2017 and saved in the variable as an integer
 
 date = input("Enter birth date (1-31)\n") #3 prompts the user to enter day of the month he was born
 
@@ -36,11 +36,16 @@ month_names = ['January',     #7 creating a list of months of the year
 
 age = int(input("How old are you now? \n")) #8 prompting the user for his age and the value being converted into an integer
 
-vari1 = month_names[month-1] #9 indexing from the month_names list for the month the user was born
-vari2 = int(date)
-vari3 = (cur_year-age)
-vari4 = date+endings[vari2-1]
-vari5 = calendar.weekday(vari3,month,vari2)
-vari6 = days[vari5]
+born_month = month_names[month-1] #9 indexing from the month_names list for the month the user was born
 
-print("You were born on",vari6 ,vari4, vari1,"of the year " ,vari3)
+date_int = int(date) #10 converting the day of birth to an integer data type
+
+year_born = (cur_year-age) #11 calculating the year the user was born
+
+conc_date = date+endings[date_int-1] #12 concatenating the date the user was born with the respective ending 
+
+day_num = calendar.weekday(year_born,month,date_int) #13 Getting the number of the day corresponding to the given date arguments. Eg Monday is 0, Tuesday is 1....
+
+part_day = days[day_num] #14 indexing from the days list the particular name of the day we have got from step 13
+
+print("You were born on",part_day ,conc_date, born_month,"of the year " ,year_born)
